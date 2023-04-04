80a49aa428bace416e8c4559e055bc91df5cf53e
Will Chapman <will.chapman@tapa.cc>
2023-04-04 19:57:55 -0400

    Converted template resource into function call

--- lc.tf
+++ lc.tf
@@ -9,10 +9,21 @@ resource "aws_launch_configuration" "jenkins" {
   iam_instance_profile = var.iam_instance_profile
   security_groups      = var.security_groups
   enable_monitoring    = var.enable_monitoring
-  user_data            = var.custom_userdata != "" ? var.custom_userdata : data.template_file.user_data.rendered
+  user_data            = var.custom_userdata != "" ?
+  var.custom_userdata :
+  templatefile("${path.module}/userdata.sh", {
+    appliedhostname         = var.hostname_prefix
+    domain_name             = var.domain_name
+    environment             = var.environment
+    efs_dnsname             = aws_efs_file_system.this.dns_name
+    preliminary_user_data   = var.preliminary_user_data
+    supplementary_user_data = var.supplementary_user_data
+    TIMEZONE                = var.timezone
+  })
 
   # Setup root block device
-  root_block_device {
+  root
+  _block_device {
     volume_size = var.volume_size
     volume_type = var.volume_type
     encrypted   = var.encrypted
@@ -23,21 +34,3 @@ resource "aws_launch_configuration" "jenkins" {
     create_before_destroy = true
   }
 }
-
-# --------------------------
-# Render userdata bootstrap file
-# --------------------------
-data "template_file" "user_data" {
-  template = file("${path.module}/userdata.sh")
-
-  vars = {
-    appliedhostname         = var.hostname_prefix
-    domain_name             = var.domain_name
-    environment             = var.environment
-    efs_dnsname             = aws_efs_file_system.this.dns_name
-    preliminary_user_data   = var.preliminary_user_data
-    supplementary_user_data = var.supplementary_user_data
-    TIMEZONE                = var.timezone
-  }
-}
-
