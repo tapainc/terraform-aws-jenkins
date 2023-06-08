2139dafe0cfd293b56ae5181d449d3fcb1c4c39e
Will Chapman <will.chapman@tapa.cc>
2023-06-08 07:31:51 -0400

    Converting aws_launch_configuration to aws_launch_template

--- asg.tf
+++ asg.tf
@@ -2,16 +2,20 @@
 # Jenkins Auto-Scaling Group
 # --------------------------
 resource "aws_autoscaling_group" "jenkins" {
-  depends_on = [aws_efs_file_system.this, aws_launch_configuration.jenkins]
+  depends_on = [aws_efs_file_system.this, aws_launch_template.jenkins]
 
-  name                      = "${var.environment}_jenkins_asg"
+  name                      = "${var.environment}-jenkins-asg"
   max_size                  = var.max_size
   min_size                  = var.min_size
   desired_capacity          = var.desired_capacity
-  launch_configuration      = aws_launch_configuration.jenkins.name
   vpc_zone_identifier       = var.vpc_zone_identifier
   health_check_grace_period = var.health_check_grace_period
   health_check_type         = var.health_check_type
+
+  launch_template {
+    id      = aws_launch_template.jenkins.id
+    version = aws_launch_template.jenkins.latest_version
+  }
 
   lifecycle {
     ignore_changes = [load_balancers, target_group_arns]
---
+++ launch_template.tf
@@ -0,0 +1,35 @@
+# --------------------------
+# Launch Config
+# --------------------------
+resource "aws_launch_template" "jenkins" {
+  name_prefix = "jenkins-lc-"
+
+  image_id      = var.ami
+  instance_type = var.instance_type
+
+  key_name               = var.key_name
+  vpc_security_group_ids = var.security_groups
+
+  iam_instance_profile {
+    name = var.iam_instance_profile
+  }
+
+  monitoring {
+    enabled = var.enable_monitoring
+  }
+
+  user_data = var.custom_userdata != "" ? var.custom_userdata : templatefile("${path.module}/userdata.sh", {
+    appliedhostname         = var.hostname_prefix
+    domain_name             = var.domain_name
+    environment             = var.environment
+    efs_dnsname             = aws_efs_file_system.this.dns_name
+    preliminary_user_data   = var.preliminary_user_data
+    supplementary_user_data = var.supplementary_user_data
+    TIMEZONE                = var.timezone
+  })
+
+  lifecycle {
+    create_before_destroy = true
+  }
+
+}
